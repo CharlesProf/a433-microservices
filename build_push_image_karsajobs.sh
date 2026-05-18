9f73cdd6d361573d530569c408ebd60e1f6fe248
CharlesProf <charles.wilbert2408@gmail.com>
2026-05-18 21:15:41 +0700

    Add GitHub Actions CI for backend

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Build Docker image untuk backend karsajobs
+# Script untuk build dan push Docker image backend karsajobs ke Docker Hub
+
+# Build Docker image dari Dockerfile yang ada di direktori ini
+# Tag image dengan format <username-docker>/karsajobs:latest
 docker build -t charlesprof/karsajobs:latest .
 
-# Login ke Docker Hub menggunakan environment variable
+# Login ke Docker Hub menggunakan password dari environment variable
+# PASSWORD_DOCKER_HUB harus di-set sebelum menjalankan script ini
+# Contoh: export PASSWORD_DOCKER_HUB=<password_kamu>
 echo $PASSWORD_DOCKER_HUB | docker login -u charlesprof --password-stdin
 
-# Push image ke Docker Hub
+# Push image yang sudah di-build ke Docker Hub
+# Image akan tersedia di: https://hub.docker.com/r/charlesprof/karsajobs
 docker push charlesprof/karsajobs:latest
-
