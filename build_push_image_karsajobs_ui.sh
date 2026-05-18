ac005df1d83119472f02c26aff0d83007fd1ce7c
CharlesProf <charles.wilbert2408@gmail.com>
2026-05-18 22:03:20 +0700

    Add GitHub Actions CI for frontend

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Build Docker image untuk frontend karsajobs-ui
+# Script untuk build dan push Docker image frontend karsajobs-ui ke Docker Hub
+
+# Build Docker image dari Dockerfile yang ada di direktori ini
+# Tag image dengan format <username-docker>/karsajobs-ui:latest
 docker build -t charlesprof/karsajobs-ui:latest .
 
-# Login ke Docker Hub menggunakan environment variable
+# Login ke Docker Hub menggunakan password dari environment variable
+# PASSWORD_DOCKER_HUB harus di-set sebelum menjalankan script ini
+# Contoh: export PASSWORD_DOCKER_HUB=<password_kamu>
 echo $PASSWORD_DOCKER_HUB | docker login -u charlesprof --password-stdin
 
-# Push image ke Docker Hub
+# Push image yang sudah di-build ke Docker Hub
+# Image akan tersedia di: https://hub.docker.com/r/charlesprof/karsajobs-ui
 docker push charlesprof/karsajobs-ui:latest
